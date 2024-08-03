c52fce93ebbf36b004dcadbe5ff5a7f9c8b88948
Pedro Ramirez <pedroluisramirezo@gmail.com>
2024-08-03 14:25:09 -0400

    Agregado layout para registrar en evento y modificaciones visuales en algunas activities

--- app/src/main/java/com/example/proyectoprogmovil/DetallesDeEvento.kt
+++ app/src/main/java/com/example/proyectoprogmovil/DetallesDeEvento.kt
@@ -1,6 +1,9 @@
 package com.example.proyectoprogmovil
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,10 +11,35 @@ import androidx.core.view.WindowInsetsCompat
 
 class DetallesDeEvento : AppCompatActivity() {
 
+    private lateinit var btnMapaCampus : Button
+    private lateinit var btnRegistrarse: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalles_de_evento)
 
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        btnMapaCampus = findViewById(R.id.btnCampusMapa)
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+    }
+
+    private fun initListeners() {
+        btnMapaCampus.setOnClickListener {
+            val intent = Intent(this, MapaCampusActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegistrarse.setOnClickListener { showForm() }
+    }
+
+    private fun showForm() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_register)
+
+        dialog.show()
     }
 }
